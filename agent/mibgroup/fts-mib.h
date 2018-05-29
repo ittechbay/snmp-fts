ede6c5b0789303c2569e49ee311cccc38bfa4c5a
ittechbay <ittechbay@163.com>
2018-05-29 22:52:35 +0800

    copy all file
    copy all file

---
+++ agent/mibgroup/fts-mib.h
@@ -0,0 +1,16 @@
+/*
+ * module to include the modules
+ */
+//config_require(fts-mib/ftsRefCfg)
+config_require(fts-mib/fts_scalar)
+config_require(fts-mib/ftsRefTable)
+config_require(fts-mib/ftsPtpTable)
+config_require(fts-mib/ftsNtpTable)
+config_require(fts-mib/ftsTimingTable)
+config_require(fts-mib/ftsClkTrap)
+
+
+
+									
+
+config_add_mib(FTS-GLOBAL-REG)
